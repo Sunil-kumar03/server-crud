a489aab77311d9c8ef114618d379d7cdc2c3ecc8
Sunil-kumar03 <challasunil2002@gmail.com>
2023-06-06 15:05:35 +0530

    update categories and products

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import Menu from "./components/Menu";
+import Home from "./components/Home";
+import Create from "./components/Create";
+import Update from "./components/Update";
+import Pnf from "./components/Pnf";
+import Products from "./components/screens/Products";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+  return(
+    <BrowserRouter>
+      <Menu/>
+      <ToastContainer autoClose={400} position={"top-right"} />
+      <Routes>
+        <Route path={`/`} element={<Home/>} />
+        <Route path={`/create`} element={<Create/>} />
+        <Route path={`/category/:cName/products`} element={<Products/>} />
+        <Route path={`/update/:id`} element={<Update/>} />
+        <Route path={`/*`} element={<Pnf/>} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
